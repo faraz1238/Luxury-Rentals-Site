b0038d4725f7b2dba4afb4fb5bb51172067e4cc5
FARAZ TANWIR <126073683+faraz1238@users.noreply.github.com>
2024-05-23 18:55:18 +0530

    first commit

---
+++ src/components/Villa/SingleVilla.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { villas } from "../../villas";
+const SingleVilla = () => {
+  const { id } = useParams();
+  const numericId = Number(id);
+  const filteredVilla = villas.filter((villa) => villa.id === numericId)[0];
+  return (
+    <>
+      <section id="singleVilla" className="page">
+        <div className="container">
+          <h3>{filteredVilla.name}</h3>
+          <div className="images">
+            <div className="villaImg">
+              <img src={filteredVilla.image} alt={filteredVilla.name} />
+            </div>
+            <div className="otherImgs">
+              <div>
+                <img src={"/landing.jpg"} alt="villa" />
+                <img src={"/people.jpg"} alt="villa" />
+              </div>
+              <div>
+                <img src={"/people2.jpg"} alt="villa" />
+                <img src={"/villa10.jpg"} alt="villa" />
+              </div>
+            </div>
+          </div>
+          <h4>{filteredVilla.location}</h4>
+          <p>
+            {filteredVilla.bathrooms} Bedrooms / {filteredVilla.guests} Guests /{" "}
+            {filteredVilla.bathrooms} Bathrooms / {filteredVilla.squareMeter}{" "}
+            Area
+          </p>
+          <div className="checkin_out">
+            <h5>
+              Check In: <span> 9:00 AM</span>
+            </h5>
+            <h5>
+              Check Out <span> 11:00 PM</span>
+            </h5>
+          </div>
+          <div className="location">
+            <h4>LOCATION</h4>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14723.614394363517!2d88.36389545!3d22.5726466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0277bb8e0b8b7b%3A0x2258c3e6f9a7956f!2sKolkata%2C%20West%20Bengal%2C%20India!5e0!3m2!1sen!2s!4v1620845226601!5m2!1sen!2s"
+              style={{ width: "100%", height: "400px", border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default SingleVilla;
